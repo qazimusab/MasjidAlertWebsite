4c380c51b5cca82c2bc99ec63fe10f706091bb42
Qazi Musab <qazi.musab@trendoidtechnologies.com>
2016-10-16 13:17:06 -0400

    latest

--- src/app/pages/salatTimes/salatTimes.module.js
+++ src/app/pages/salatTimes/salatTimes.module.js
@@ -10,6 +10,7 @@
             .state('salatTimes', {
                 url: '/salatTimes',
                 templateUrl: 'app/pages/salatTimes/salat-times.html',
+                controller: 'SalatTablesPageCtrl',
                 title: 'Salat Times',
                 sidebarMeta: {
                     icon : 'fa fa fa-bullhorn',
@@ -17,5 +18,38 @@
                 },
             });
     }
+
+
+
+    //function routeConfig($stateProvider, $urlRouterProvider) {
+    //    $stateProvider
+    //        .state('tables', {
+    //            url: '/tables',
+    //            template : '<ui-view></ui-view>',
+    //            abstract: true,
+    //            controller: 'TablesPageCtrl',
+    //            title: 'Tables',
+    //            sidebarMeta: {
+    //                icon: 'ion-grid',
+    //                order: 300,
+    //            },
+    //        }).state('tables.basic', {
+    //        url: '/basic',
+    //        templateUrl: 'app/pages/tables/basic/tables.html',
+    //        title: 'Basic Tables',
+    //        sidebarMeta: {
+    //            order: 0,
+    //        },
+    //    }).state('tables.smart', {
+    //        url: '/smart',
+    //        templateUrl: 'app/pages/tables/smart/tables.html',
+    //        title: 'Smart Tables',
+    //        sidebarMeta: {
+    //            order: 100,
+    //        },
+    //    });
+    //    $urlRouterProvider.when('/tables','/tables/basic');
+    //}
+
 
 })();
